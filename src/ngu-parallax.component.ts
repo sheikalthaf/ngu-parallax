04108aa65d1d95712c0f6afdc181dac2d8110df8
sheikalthaf <sheikalthaf@outlook.com>
2017-11-09 23:13:56 +0530

    imageurl sanitizing added

--- src/ngu-parallax.component.ts
+++ src/ngu-parallax.component.ts
@@ -8,11 +8,13 @@ import {
   ViewChild,
   ElementRef
 } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'ngu-parallax',
   template: `<img #img [src]="image" (load)="isLoaded($event)"><div><ng-content></ng-content></div>`,
-  styles: [`
+  styles: [
+    `
     :host {
         display: block;
         overflow: hidden;
@@ -32,22 +34,29 @@ import {
         top: 0px;
         left: 0px;
     }
-  `]
+  `
+  ]
 })
 export class NguParallaxComponent implements OnInit {
+  _imgSafe: SafeUrl;
   imgToBeScroll: number;
   imgheit: number;
   parallaxOffestTop: any;
   windowHeight: number;
   parallaxHeight: any;
   img: any;
-  @Input('image') image: string;
+  // tslint:disable-next-line:no-input-rename
+  @Input('image') imagedata: string;
   @ViewChild('img', { read: ElementRef })
   img1: ElementRef;
-  transformImg = 0;
+  // transformImg = 0;
   imageHeight: number;
 
-  constructor(private renderer: Renderer, private el: ElementRef) {}
+  constructor(
+    private renderer: Renderer,
+    private el: ElementRef,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.img = this.img1.nativeElement;
@@ -56,18 +65,23 @@ export class NguParallaxComponent implements OnInit {
       this.onWindowScrolling()
     );
     this.windowHeight = window.innerHeight;
+    this._imgSafe = this.sanitizer.bypassSecurityTrustUrl(this.imagedata || '');
     // console.log(this.parallaxOffestTop, this.windowHeight, this.parallaxHeight);
+  }
+
+  public get image() {
+    return this._imgSafe;
   }
 
   public isLoaded(event: Event) {
     this.imageHeight = this.img.offsetHeight;
     this.parallaxOffestTop = this.el.nativeElement.offsetTop;
     this.imgheit = Math.round(
-        (this.imageHeight - this.parallaxHeight) / this.imageHeight * 100
-      );
-      this.imgToBeScroll = Math.round(
-        this.parallaxHeight / this.imageHeight * 100
-      );
+      (this.imageHeight - this.parallaxHeight) / this.imageHeight * 100
+    );
+    this.imgToBeScroll = Math.round(
+      this.parallaxHeight / this.imageHeight * 100
+    );
     // console.log(this.parallaxOffestTop, this.windowHeight, this.parallaxHeight)
     // console.log(this.imageHeight);
   }
@@ -78,7 +92,7 @@ export class NguParallaxComponent implements OnInit {
     const cond =
       this.parallaxOffestTop <= scrollY + this.windowHeight &&
       this.parallaxOffestTop + this.parallaxHeight >= scrollY;
-      // console.log(this.el.nativeElement.offsetTop, this.parallaxOffestTop);
+    // console.log(this.el.nativeElement.offsetTop, this.parallaxOffestTop);
     if (cond) {
       const minss =
         this.parallaxOffestTop > this.windowHeight
@@ -86,7 +100,9 @@ export class NguParallaxComponent implements OnInit {
           : 0;
       const tot = scrollY - minss;
       // const cond = tot <= this.imageHeight;
-      const result = Math.round(tot / (this.windowHeight + this.parallaxHeight) * 100);
+      const result = Math.round(
+        tot / (this.windowHeight + this.parallaxHeight) * 100
+      );
       const finalPer = Math.round(result / 100 * this.imgToBeScroll);
       // console.log(cond, tot, this.imgToBeScroll, result, finalPer);
       // console.log(this.windowHeight)
@@ -99,9 +115,4 @@ export class NguParallaxComponent implements OnInit {
       );
     }
   }
-
 }
-
-/**
- * scroll - window height and parallax div height with image height
- */
